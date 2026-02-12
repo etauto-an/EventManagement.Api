2f17fc22d755f460982cd00594b46b88883bfbf5
etauto-an <etauto-an@csu.fullerton.edu>
2026-02-11 21:29:04 -0800

    added Repositories files

--- EventManagement.Api/obj/Debug/net8.0/EventManagement.Api.AssemblyInfo.cs
+++ EventManagement.Api/obj/Debug/net8.0/EventManagement.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93833e4dcc25dcc027457e24e08310474209abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
